7e2be6ab2dc2bd3caceb37079cd250ec25733a18
khg <winquake3@hanmail.net>
2024-05-07 02:06:28 +0900

    2가지 예외 작성

--- khg/src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
+++ khg/src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -1,19 +1,20 @@
 package kr.co.shortenurlservice.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import kr.co.shortenurlservice.domain.LackOfShortenUrlKeyException;
+import kr.co.shortenurlservice.domain.NotFoundShortenUrlException;
 import kr.co.shortenurlservice.domain.ShortenUrl;
 import kr.co.shortenurlservice.domain.ShortenUrlRepository;
 import kr.co.shortenurlservice.presentation.ShortenUrlCreateRequestDto;
 import kr.co.shortenurlservice.presentation.ShortenUrlCreateResponseDto;
 import kr.co.shortenurlservice.presentation.ShortenUrlInformationDto;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 @Service
 public class SimpleShortenUrlService {
     private ShortenUrlRepository shortenUrlRepository;
-
+    
     @Autowired
     SimpleShortenUrlService(ShortenUrlRepository shortenUrlRepository) {
         this.shortenUrlRepository = shortenUrlRepository;
@@ -25,8 +26,24 @@ public class SimpleShortenUrlService {
         //1.원래의 URL 가져오기
         String originalUrl = shortenUrlCreateRequestDto.getOriginalUrl();
 
-        //2. 단축 URL 키  생성
-        String shortenUrlKey = ShortenUrl.generateShortenUrlKey();
+        
+        String shortenUrlKey = "";
+
+        final int MAX_RETRY_COUNT = 5;
+        int count = 0;
+
+       //단축 URL 중복 체크
+        while(count++ < MAX_RETRY_COUNT) {
+             //2. 단축 URL 생성 후 
+            shortenUrlKey = ShortenUrl.generateShortenUrlKey();
+            ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
+            if(null == shortenUrl) //단축 URL이 없으면 
+                break;
+        }
+
+        //2-1. 중복이 5번 이상 발생하면 예외 발생
+        if(count > MAX_RETRY_COUNT)
+            throw new LackOfShortenUrlKeyException();
 
         //3. 원래의 URL과 단축 URL 키를 통해 ShortenUrl 도메인 객체 생성
         ShortenUrl shortenUrl = new ShortenUrl(originalUrl, shortenUrlKey);
@@ -43,6 +60,9 @@ public class SimpleShortenUrlService {
     public String getOriginalUrlByShortenUrlKey(String shortenUrlKey) {
         ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
 
+        if(null == shortenUrl)
+            throw new NotFoundShortenUrlException();
+
         shortenUrl.increaseRedirectCount();
         shortenUrlRepository.saveShortenUrl(shortenUrl);
 
@@ -53,6 +73,9 @@ public class SimpleShortenUrlService {
 
     public ShortenUrlInformationDto getShortenUrlInformationByShortenUrlKey(String shortenUrlKey) {
         ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
+
+        if(null == shortenUrl)
+            throw new NotFoundShortenUrlException();
 
         ShortenUrlInformationDto shortenUrlInformationDto = new ShortenUrlInformationDto(shortenUrl);
 
